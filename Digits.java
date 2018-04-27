b2a510a8fd07d1898bd5332dec75f07e5557b4ae
AbdulrhmanAlFawzan <38785225+AbdulrhmanAlFawzan@users.noreply.github.com>
2018-04-27 15:01:46 +0300

    Uplouded a java file

---
+++ Digits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Digits {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number (-1 to end): ");
+		int num = s.nextInt();
+		int digit;
+		int counter = 0;
+		while (num != -1) {
+			while (num != 0) {
+				counter++;
+				digit = num % 10;
+				System.out.println("digit" + counter + " = " + digit);
+				num = num / 10;
+			}
+			System.out.print("Enter a number (-1 to end): ");
+			num = s.nextInt();
+			counter = 0;
+		}
+	}
+
+}
